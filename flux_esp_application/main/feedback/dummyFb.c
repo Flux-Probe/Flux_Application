80fea808f268569665ad87e4cd3c487e9ef117f3
Nicholas <krsinich589@gmail.com>
2026-05-23 15:34:05 -0400

    Adding dummy Feedback module to work isolated

---
+++ flux_esp_application/main/feedback/dummyFb.c
@@ -0,0 +1,42 @@
+
+#include "feedback.h"
+#include "esp_timer.h"
+
+#define TAG "DUMMY_FB"
+
+typedef struct {
+    uint16_t maxFbVal;
+} dummyFbCtx_t;
+
+static resp_t readDummyData(feedback_t *fb, float *readVal)
+{
+    CHECK_PTR_RET_ERR(fb);
+    dummyFbCtx_t *cfg = (dummyFbCtx_t *)fb->privCtx;
+    CHECK_PTR_RET_ERR(cfg);
+    int timeMs = (long long) esp_timer_get_time() / 1000;
+    *readVal = timeMs % cfg->maxFbVal;
+    return RESP_OK;
+}
+
+static resp_t resetDummyData(feedback_t *fb, float resetVal)
+{
+    return RESP_OK;
+}
+
+feedback_t *dummyFbInit(int maxTime)
+{
+
+    dummyFbCtx_t *dummyPrivCtx = (dummyFbCtx_t *) calloc(1, sizeof(dummyFbCtx_t));
+    feedback_t *dummyFb = (feedback_t *) calloc(1, sizeof(feedback_t));
+    if (!dummyFb || !dummyPrivCtx) {
+        return NULL;
+    }
+    dummyPrivCtx->maxFbVal = maxTime;
+
+    dummyFb->privCtx = (void *)dummyPrivCtx;
+    dummyFb->readData = readDummyData;
+    dummyFb->readRawData = readDummyData;
+    dummyFb->resetData = resetDummyData;
+
+    return dummyFb;
+}
---
+++ flux_esp_application/main/feedback/dummyFb.h
@@ -0,0 +1,7 @@
+#ifndef _DUMMY_FB
+#define _DUMMY_FB
+#include "feedback.h"
+
+feedback_t *dummyFbInit(int maxTime);
+
+#endif
